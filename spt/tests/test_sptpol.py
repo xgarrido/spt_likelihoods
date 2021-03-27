01400b1d009a6a0e09f7e9a27a8dd70d4bb534e3
Xavier Garrido <xavier.garrido@gmail.com>
2021-03-27 01:43:44 +0100

    kozmetix

--- spt/tests/test_sptpol.py
+++ spt/tests/test_sptpol.py
@@ -8,8 +8,32 @@ packages_path = os.environ.get("COBAYA_PACKAGES_PATH") or os.path.join(
     tempfile.gettempdir(), "SPT_packages"
 )
 
+cosmo_params = dict(
+    cosmomc_theta=0.010411,
+    As=1e-10 * np.exp(3.1),
+    ombh2=0.0221,
+    omch2=0.1200,
+    ns=0.96,
+    Alens=1.0,
+    tau=0.09,
+)
 
-class SPTPolLiteTest(unittest.TestCase):
+fg_params = dict(
+    kappa=0.0,
+    czero_psTE_150=0.0,
+    czero_psEE_150=0.0837416,
+    ADust_TE=0.1647,
+    ADust_EE=0.0236,
+    alphaDust_TE=-2.42,
+    alphaDust_EE=-2.42,
+    mapTcal=1.0,
+    mapPcal=1.0,
+    beam1=0.0,
+    beam2=0.0,
+)
+
+
+class SPTPolTest(unittest.TestCase):
     def setUp(self):
         from cobaya.install import install
 
@@ -18,30 +42,6 @@ class SPTPolLiteTest(unittest.TestCase):
     def test_cobaya(self):
         """Test the Cobaya interface to the SPTPol likelihood."""
         from cobaya.model import get_model
-
-        cosmo_params = dict(
-            cosmomc_theta=0.010411,
-            As=1e-10 * np.exp(3.1),
-            ombh2=0.0221,
-            omch2=0.1200,
-            ns=0.96,
-            Alens=1.0,
-            tau=0.09,
-        )
-
-        fg_params = dict(
-            kappa=0.0,
-            czero_psTE_150=0.0,
-            czero_psEE_150=0.0837416,
-            ADust_TE=0.1647,
-            ADust_EE=0.0236,
-            alphaDust_TE=-2.42,
-            alphaDust_EE=-2.42,
-            mapTcal=1.0,
-            mapPcal=1.0,
-            beam1=0.0,
-            beam2=0.0,
-        )
 
         info = {
             "debug": True,
